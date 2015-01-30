e70406da47813cf3b591eb1c4134e3de4811142c
Stav <stav@ftbpro.com>
2015-01-30 20:58:54 +0200

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SportsproRepo::Application.config.secret_token = 'd051570b73a34aa751fe86a9b7637b2c3b2ed7dae1faf5d554441368ad9d29d51f5d49c34c8354eff078b53dba90274b3aba3f3b5cff2603191429b67cb5eca9'
